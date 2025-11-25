84738e220680c5ccfb5cf827e089ea2906a35110
cyrus4u <cyrus4u@users.noreply.github.com>
2025-11-25 18:33:49 +0330

    Internationalizing Flutter apps

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,9 @@ import 'package:cryptoapp/provider/theme_provider.dart';
 import 'package:cryptoapp/ui/ui_helper/theme_switcher.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:provider/provider.dart';
+import 'l10n/app_localizations.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -25,26 +27,45 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return Consumer<ThemeProvider>(builder: (context,themeProvider,child){
-      return MaterialApp(
-        themeMode: themeProvider.themeMode,
-        theme: MyThemes.lightTheme,
-        darkTheme: MyThemes.darkTheme,
-      debugShowCheckedModeBanner: false,
-      home: Directionality(
-        textDirection: TextDirection.ltr,
-        child: Scaffold(
-          appBar: AppBar(
-            title: Text('Exchange', style: TextStyle(color: Colors.white)),
-            backgroundColor: Colors.blue,
-            centerTitle: true,
-            actions: [ThemeSwitcher()],
+    return Consumer<ThemeProvider>(
+      builder: (context, themeProvider, child) {
+        return MaterialApp(
+          locale: Locale('en'),
+          localizationsDelegates: [
+            AppLocalizations.delegate, // Add this line
+            GlobalMaterialLocalizations.delegate,
+            GlobalWidgetsLocalizations.delegate,
+            GlobalCupertinoLocalizations.delegate,
+          ],
+          supportedLocales: [
+            Locale('en'), // English
+            Locale('fa'), // Farsi
+          ],
+          themeMode: themeProvider.themeMode,
+          theme: MyThemes.lightTheme,
+          darkTheme: MyThemes.darkTheme,
+          debugShowCheckedModeBanner: false,
+          home: Builder(
+            builder: (context) {
+              return Directionality(
+                textDirection: TextDirection.ltr,
+                child: Scaffold(
+                  appBar: AppBar(
+                    title: Text(
+                      'Exchange',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    backgroundColor: Colors.blue,
+                    centerTitle: true,
+                    actions: [ThemeSwitcher()],
+                  ),
+                  body: Container(child: Center(child: Text(AppLocalizations.of(context)!.helloWorld)),),
+                ),
+              );
+            },
           ),
-          body: Container(),
-        ),
-      ),
+        );
+      },
     );
-    });
-    
   }
 }
