d64057d9e9fdba3bd862d5f270579ac4840517a1
cyrus4u <cyrus4u@users.noreply.github.com>
2025-11-25 11:29:20 +0330

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:cryptoapp/ui/ui_helper/theme_switcher.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Directionality(
+        textDirection: TextDirection.ltr,
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text('Exchange', style: TextStyle(color: Colors.white)),
+            backgroundColor: Colors.blue,
+            centerTitle: true,
+            actions: [ThemeSwitcher()],
+          ),
+          body: Container(),
+        ),
+      ),
+    );
+  }
+}
